d3cc8d281863423fff1b744fbb8cfe2259181eca
Feryal Doubali <feryalhalima@gmail.com>
2024-04-01 15:59:07 +0200

    side et nav bar fixed

--- src/Components/Loader/Loader.jsx
+++ src/Components/Loader/Loader.jsx
@@ -1,11 +1,13 @@
-import React from "react";
+//import React from "react";
 import "./Loader.css";
 import menhirImage from "../../assets/menhir.png";
 
 const Loader = () => (
   <div className="loader">
     <img src={menhirImage} alt="Menhir" className="menhir-image" />
-    <p>Loading...</p>
+    <div className="loader-message">
+      <p>Loading...</p>
+    </div>
   </div>
 );
 
