6e38af65587eb9f7ec8a18a3bf257e4cd83d78e1
Alexis <alexismoreauavila@gmail.com>
2024-04-05 12:37:45 +0200

    yeah

--- src/Components/AddCommentsButton/AddCommentsButton.jsx
+++ src/Components/AddCommentsButton/AddCommentsButton.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { useState } from "react";
+import axios from "axios";
+
 function AddCommentsButton({ id }) {
   const [description, setDescription] = useState(null);
+  async function sendComment() {
+    try {
+      let response = await axios.post();
+    } catch (error) {
+      console.log(error);
+    }
+  }
   return (
     <div>
-      <label htmlFor="">write a comment</label>
-      <br />
-      <input type="text" />
+      <form onSubmit={() => sendComment()}>
+        <label htmlFor="">write a comment</label>
+        <br />
+        <input type="text" />
+      </form>
     </div>
   );
 }
