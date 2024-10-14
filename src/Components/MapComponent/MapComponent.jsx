933d17642499e9a54179c253b73b103a7bca9411
pedro <alexismoreauavila@gmail.com>
2024-10-14 10:29:30 +0200

    fixing URI

--- src/Components/MapComponent/MapComponent.jsx
+++ src/Components/MapComponent/MapComponent.jsx
@@ -25,8 +25,7 @@ let url = `${import.meta.env.VITE_BACKEND_URL}/megalith`;
 
 // Custom Red Marker Icon
 const redIcon = new L.Icon({
-  iconUrl:
-    "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-red.png",
+  iconUrl: "https://example.com/path/to/your/red-marker-icon.png", // Replace with a working URL
   iconSize: [25, 41],
   iconAnchor: [12, 41],
   popupAnchor: [1, -34],
